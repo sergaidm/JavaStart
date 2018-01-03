aa480c1c68aaa8d62bd1935c7b4b517b4367725f
sergaidm <sergaidm@yandex.ru>
2018-01-03 12:07:04 +0200

    Exercise312

--- Exercise312/src/ru/yandex/sergaidm/Main.java
+++ Exercise312/src/ru/yandex/sergaidm/Main.java
@@ -6,29 +6,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int floor = 9;
-		int frontDoor = 4;
-		int flatsOnTheFloor = 4;
-		int numberOfFlats = floor * frontDoor * flatsOnTheFloor;
 		int flat;
+		int frontDoor;
+		int floor;
 		System.out.println("Vvedite nomer kvartiry flat = ");
 		flat = sc.nextInt();
-		if (1 <= flat & flat <= (floor * flatsOnTheFloor)) {
-			System.out.println("Kvartira raspolojena v 1 pod'ezde");
-		}
-		if ((floor * flatsOnTheFloor) + 1 <= flat & flat <= (2 * (floor * flatsOnTheFloor))) {
-			System.out.println("Kvartira raspolojena vo 2 pod'ezde");
-		}
-		if ((2 * (floor * flatsOnTheFloor) + 1) <= flat & flat <= (3 * (floor * flatsOnTheFloor))) {
-			System.out.println("Kvartira raspolojena v 3 pod'ezde");
-		}
-		if ((3 * (floor * flatsOnTheFloor) + 1) <= flat & flat <= (4 * (floor * flatsOnTheFloor))) {
-			System.out.println("Kvartira raspolojena v 4 pod'ezde");
-		}
 
-		
+		if (flat >= 1 & flat <= 4 * 4 * 9) {
+			frontDoor = ((flat - 1) / (9 * 4)) + 1;
+			flat = flat - 36 * (frontDoor - 1);
+			floor = (flat + 3) / 4;
 
-		if (flat > numberOfFlats || flat < 1) {
+			System.out.println("Kvartira raspolojena v pod'ezde " + frontDoor);
+			System.out.println("Kvartira raspolojena na etaje " + floor);
+
+		} else {
 			System.out.println("V etom dome takoi kvartiry net");
 		}
 
