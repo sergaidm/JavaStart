f1514f289b68996b1a39666606fc888ffc5416f0
sergaidm <sergaidm@yandex.ru>
2018-01-06 17:55:52 +0200

    Exercise422

---
+++ Exercise422/src/ru/yandex/sergaidm/Main.java
@@ -0,0 +1,16 @@
+package ru.yandex.sergaidm;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			if ((i % 2) == 0 & (i / 2) == 1 || (i % 3) == 0 & (i / 3) == 1 || (i % 5) == 0 & (i / 5) == 1
+					|| (i % 7) == 0 & (i / 7) == 1) {
+			} else if (i == 1 || (i % 2) == 0 || (i % 3) == 0 || (i % 4) == 0 || (i % 5) == 0 || (i % 6) == 0
+					|| (i % 7) == 0 || (i % 8) == 0 || (i % 9) == 0) {
+				continue;
+			}
+			System.out.print(i + " ");
+		}
+	}
+}
