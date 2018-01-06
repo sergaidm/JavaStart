6aae2456fe390ffccc68d739e20d49f036613e25
sergaidm <sergaidm@yandex.ru>
2018-01-06 17:30:46 +0200

    Exercise322

--- Exercise322/src/ru/yandex/sergaidm/Main.java
+++ Exercise322/src/ru/yandex/sergaidm/Main.java
@@ -13,41 +13,17 @@ public class Main {
 		double by = 4;
 		double cx = 6;
 		double cy = 1;
-		double u11;
-		double u12;
-		double u21;
-		double u22;
-		double u31;
-		double u32;
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Vvedite koordinaty x i y:");
 		x = sc.nextDouble();
 		y = sc.nextDouble();
 
-		/* Vychislim uravneniya pryamyh, obrazovyvayushchih treugol'nik */
+		double a = (ax - x) * (by - ay) - (bx - ax) * (ay - y);
+		double b = (bx - x) * (cy - by) - (cx - bx) * (by - y);
+		double c = (cx - x) * (ay - cy) - (ax - cx) * (cy - y);
 
-		u11 = (x - ax) / (by - ay);
-		u12 = (y - ay) / (by - ay);
-
-		if (u11 == u12) {
-			y = x;/* Uravnenie pervoi pryamoi */
-		}
-
-		u21 = (x - bx) / (cx - bx);
-		u22 = (y - by) / (cy - by);
-
-		if (u21 == u22) {
-			y = -1.5 * x + 10;/* Uravnenie vtoroi pryamoi */
-		}
-
-		u31 = (x - ax) / (cx - ax);
-		u32 = (y - cy) / (cy - ay);
-
-		if (u31 == u32) {
-			y = x / 6;/* Uravnenie tret'ei pryamoi */
-		}
-
-		if ((y <= x) & (y <= -1.5 * x + 10) & (y >= x / 6)) {
+		if (a >= 0 & b >= 0 &  c >= 0 || a <= 0 & b <= 0 & c <= 0) {
 			System.out.println("Vvedennye koordinaty lejat vnutri treugol'nika");
 		} else {
 			System.out.println("Vvedennye koordinaty lejat za predelami treugol'nika");
