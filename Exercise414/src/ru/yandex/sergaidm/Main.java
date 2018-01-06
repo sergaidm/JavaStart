308299899c2ce258abe9d0b62946d7ef8bb5eb77
sergaidm <sergaidm@yandex.ru>
2018-01-06 17:54:40 +0200

    Exercise414

---
+++ Exercise414/src/ru/yandex/sergaidm/Main.java
@@ -0,0 +1,30 @@
+package ru.yandex.sergaidm;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int w = 0;
+		int h = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Vvedite shirinu prymougol'nika");
+		w = sc.nextInt();
+		System.out.println("Vvedite vysotu prymougol'nika");
+		h = sc.nextInt();
+
+		for (int i = 1; i <= h; i++) {
+			for (int j = 1; j <= w; j++) {
+				if ((i == 1 & j > 1 & j < w) || (i == h & j > 1 & j < w)) {
+					System.out.print("*");
+				} else if (j > 1 & j < w) {
+					System.out.print(" ");
+
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
