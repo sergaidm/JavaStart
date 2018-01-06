d8026f2dfed926eb78756e9eb941d4e5c13e9b27
sergaidm <sergaidm@yandex.ru>
2018-01-06 17:53:51 +0200

    Exercise413

---
+++ Exercise413/src/ru/yandex/sergaidm/Main.java
@@ -0,0 +1,14 @@
+package ru.yandex.sergaidm;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 0;
+		for (int i = 1; i <= 9; i++) {
+			a = i * 5;
+			System.out.println(i + " x 5 " + " = " + a);
+		}
+
+	}
+
+}
