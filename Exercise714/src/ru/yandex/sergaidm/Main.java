ff05ce27a6df528f4d065a636484565f8fc63e1d
sergaidm <sergaidm@yandex.ru>
2018-01-19 19:18:54 +0200

    Exercise714

---
+++ Exercise714/src/ru/yandex/sergaidm/Main.java
@@ -0,0 +1,17 @@
+package ru.yandex.sergaidm;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		for (int i = 2; i <= 11; i++) {
+			System.out.println(getNumber("PI", Math.PI, i));
+		}
+
+	}
+
+	public static String getNumber(String constantName, double value, int n) {
+		String text = String.format(constantName + " = %." + n + "f", value);
+		return text;
+	}
+}
