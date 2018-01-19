c2b176b7bd4c4077a60aa908ddb86f4fbf2e043a
sergaidm <sergaidm@yandex.ru>
2018-01-19 19:17:13 +0200

    Exercise712

---
+++ Exercise712/src/ru/yandex/sergaidm/Main.java
@@ -0,0 +1,39 @@
+package ru.yandex.sergaidm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input size of the array: ");
+		int n = sc.nextInt();
+		int[] array = new int[n];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) (Math.random() * 100);
+		}
+
+		System.out.println("Standart version of method Arrays to String: " + Arrays.toString(array));
+		System.out.println("Own version of method Arrays to String:      " + ownVersionOfMethodArraysToString(array));
+
+	}
+
+	public static String ownVersionOfMethodArraysToString(int[] a) {
+
+		StringBuilder sb = new StringBuilder("[");
+
+		for (int i = 0; i < a.length; i++) {
+			if (i == a.length - 1) {
+				sb.append(a[i] + "]");
+			} else {
+				sb.append(a[i] + ", ");
+			}
+		}
+
+		String text = sb.toString();
+		return text;
+	}
+
+}
