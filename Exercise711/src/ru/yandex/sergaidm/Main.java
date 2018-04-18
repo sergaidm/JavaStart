8c2124aea06c333be6252db39f8119ab70c0c950
sergaidm <sergaidm@yandex.ru>
2018-04-18 19:34:45 +0300

    7. Elements of standard library. Number of milliseconds per month

--- Exercise711/src/ru/yandex/sergaidm/Main.java
+++ Exercise711/src/ru/yandex/sergaidm/Main.java
@@ -1,5 +1,6 @@
 package ru.yandex.sergaidm;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -7,24 +8,19 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Calendar cl1 = Calendar.getInstance();
-		cl1.get(Calendar.DATE);
+		Calendar clOne = Calendar.getInstance();
+		Calendar clTwo = Calendar.getInstance();
 
-		Date date1 = cl1.getTime();
-		System.out.println(date1);
-		System.out.println(date1.getTime());
+		clTwo.set(Calendar.MONTH, clOne.get(Calendar.MONTH) - 1);
 
-		Calendar cl2 = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM yyyy");
+		System.out.println(sdf.format(clOne.getTime()));
+		System.out.println(sdf.format(clTwo.getTime()));
+		
+		Date dateOne = clOne.getTime();
+		Date dateTwo = clTwo.getTime();
 
-		cl2.set(Calendar.YEAR, 2017);
-		cl2.set(Calendar.MONTH, Calendar.DECEMBER);
-		cl2.set(Calendar.DAY_OF_MONTH, 17);
-
-		Date date2 = cl2.getTime();
-		System.out.println(date2);
-		System.out.println(date2.getTime());
-
-		long number = date1.getTime() - date2.getTime();
+		long number = dateOne.getTime() - dateTwo.getTime();
 
 		System.out.println("Number of milliseconds per month = " + number);
 	}
