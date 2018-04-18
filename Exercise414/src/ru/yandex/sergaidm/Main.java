46facadf15ba22dce3349ffd1fb32e0782ba0bf9
sergaidm <sergaidm@yandex.ru>
2018-04-18 16:03:53 +0300

    4. Cycles. Rectangle

--- Exercise414/src/ru/yandex/sergaidm/Main.java
+++ Exercise414/src/ru/yandex/sergaidm/Main.java
@@ -5,26 +5,26 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		int w = 0;
 		int h = 0;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Vvedite shirinu prymougol'nika");
+		System.out.println("Input the width of rectangle: ");
 		w = sc.nextInt();
-		System.out.println("Vvedite vysotu prymougol'nika");
+		System.out.println("Input the height of rectangle: ");
 		h = sc.nextInt();
-
 		for (int i = 1; i <= h; i++) {
 			for (int j = 1; j <= w; j++) {
 				if ((i == 1 & j > 1 & j < w) || (i == h & j > 1 & j < w)) {
 					System.out.print("*");
 				} else if (j > 1 & j < w) {
 					System.out.print(" ");
-
 				} else {
 					System.out.print("*");
 				}
 			}
 			System.out.println();
 		}
+
 	}
 }
