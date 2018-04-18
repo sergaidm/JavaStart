7f06197de4aade2803b11fd093a8747327417dfd
sergaidm <sergaidm@yandex.ru>
2018-04-18 17:35:37 +0300

    6. Methods. Maximum number from array

--- Exercise611/src/ru/yandex/sergaidm/Main.java
+++ Exercise611/src/ru/yandex/sergaidm/Main.java
@@ -6,33 +6,28 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Vvedite razmer massiva n = ");
+		System.out.println("Input size of array: ");
 		int n = sc.nextInt();
 		int[] array = new int[n];
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) (Math.random() * 100);
 		}
-		System.out.println(Arrays.toString(array));
-
+		System.out.println("Base array: " + Arrays.toString(array));
 		int max = getMax(array);
-
-		System.out.println("Maximal'noe chislo massiva = " + max);
+		System.out.println("Maximum number from array: " + max);
 
 	}
 
 	public static int getMax(int[] array) {
-
 		int max = array[0];
-		
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] > max) {
 				max = array[i];
 			}
 		}
-		
 		return max;
-		
 	}
 
 }
