83992e6a3ef58535ca07cade7c6900156aa8e4c5
sergaidm <sergaidm@yandex.ru>
2018-04-18 15:07:43 +0300

    3. Conditional statements. Determination of number of days in a year

--- Exercise313/src/ru/yandex/sergaidm/Main.java
+++ Exercise313/src/ru/yandex/sergaidm/Main.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		int year;
-		System.out.println("Vvedite god");
+		System.out.println("Input a year: ");
 		year = sc.nextInt();
-
 		if (year % 4 == 0 & year % 400 == 0) {
-			System.out.println("V etom godu 366 dnei");
+			System.out.println("In this year 366 days");
 		} else if (year % 4 == 0 & year % 100 == 0) {
-			System.out.println("V etom godu 365 dnei");
+			System.out.println("In this year 365 days");
 		} else if (year % 4 == 0) {
-			System.out.println("V etom godu 366 dnei");
+			System.out.println("In this year 366 days");
 		} else {
-			System.out.println("V etom godu 365 dnei");
+			System.out.println("In this year 365 days");
 		}
 
 	}
