f8bf4c43abd5341cf318bcbcc753e596c8193cf0
sergaidm <sergaidm@yandex.ru>
2018-04-18 16:34:45 +0300

    5. Arrays. Array of integers

--- Exercise512/src/ru/yandex/sergaidm/Main.java
+++ Exercise512/src/ru/yandex/sergaidm/Main.java
@@ -6,18 +6,16 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Vvedite razmer massiva n = ");
+		System.out.println("Input size of array: ");
 		int n = sc.nextInt();
-		int[] a = new int[n];
-
-		System.out.println("Vvedite znacheniya massiva");
-		for (int i = 0; i < a.length; i++) {
-			a[i] = sc.nextInt();
+		int[] array = new int[n];
+		System.out.println("Input values of elements of the array: ");
+		for (int i = 0; i < array.length; i++) {
+			array[i] = sc.nextInt();
 		}
-
-		System.out.println(Arrays.toString(a));
-
+		System.out.println(Arrays.toString(array));
 	}
 
 }
