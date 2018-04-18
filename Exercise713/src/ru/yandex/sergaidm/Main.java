1ed963ea7ca3af5368e573bdcaf0f97ece32f2fb
sergaidm <sergaidm@yandex.ru>
2018-04-18 19:52:25 +0300

    7. Elements of standard library. Conversion of binary number to denary

--- Exercise713/src/ru/yandex/sergaidm/Main.java
+++ Exercise713/src/ru/yandex/sergaidm/Main.java
@@ -1,6 +1,5 @@
 package ru.yandex.sergaidm;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,27 +8,13 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Input a number in binary format: ");
-		String num = sc.nextLine();
-
-		char[] t = num.toCharArray();
-		
-		for (int i = t.length - 1; i >= 0; i--) {
-			char s = num.charAt(num.length() - 1 - i);
-			if (s == '1') {
-				t[i] = 1;
-
-			} else if (s == '0') {
-				t[i] = 0;
-			}
-		}
-
 		int n = 0;
-		for (int i = 0; i < t.length; i++) {
-
-			n += (int) (Math.pow(2, i)) * t[i];
+		char[] num = sc.nextLine().toCharArray();		
+		for (int i = num.length - 1; i >= 0; i--) {
+			n += Integer.valueOf("" + num[i]) << num.length - i - 1;
 		}
-
-		System.out.println("Your number in denary format: " + n);
+		String number = new String(num);
+		System.out.println("Number " + number + " in denary format: " + n);
 
 	}
 
