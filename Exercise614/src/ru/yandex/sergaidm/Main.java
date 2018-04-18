d11475122d7070b0f2c9fb16ecbac2e5e1702d3d
sergaidm <sergaidm@yandex.ru>
2018-04-18 17:53:47 +0300

    6. Methods. Searching for element in array

--- Exercise614/src/ru/yandex/sergaidm/Main.java
+++ Exercise614/src/ru/yandex/sergaidm/Main.java
@@ -6,32 +6,28 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Vvedite razmer massiva n = ");
+		System.out.println("Input size of array: ");
 		int n = sc.nextInt();
 		int[] array = new int[n];
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) (Math.random() * 100);
 		}
-		System.out.println(Arrays.toString(array));
-
-		System.out.println("Vvedite znachenie elementa massiva: ");
+		System.out.println("Base array: " + Arrays.toString(array));
+		System.out.println("Input value of array element: ");
 		int element = sc.nextInt();
-
-		System.out.println("Indeks naydennogo elementa massiva = " + findElement(array, element));
+		System.out.println("Index of the found array element: " + findElement(array, element));
 
 	}
 
-	public static int findElement(int[] array, int element) {
-		
+	public static int findElement(int[] array, int element) {		
 		int ind = -1;
 		for (int i = 0; i < array.length; i++) {
 			if (element == array[i]) {
 				ind = i;
 			}
-
 		}
-
 		return ind;
 	}
 
