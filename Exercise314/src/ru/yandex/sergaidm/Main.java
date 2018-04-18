65794bf18a1a11fe16e1955a57424fad16f75516
sergaidm <sergaidm@yandex.ru>
2018-04-18 15:18:10 +0300

    3. Conditional statements. Determination of existence  of triangle

--- Exercise314/src/ru/yandex/sergaidm/Main.java
+++ Exercise314/src/ru/yandex/sergaidm/Main.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		double a;
 		double b;
 		double c;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Vvedite storony treugol'nika:");
+		System.out.println("Input sides of triangle : ");
 		a = sc.nextDouble();
 		b = sc.nextDouble();
 		c = sc.nextDouble();
 		if (((a + b) > c) & ((a + c) > b) & ((b + c) > a)) {
-			System.out.println("Takoi treugol'nik sushchestvuet");
+			System.out.println("This triangle is exists");
 		} else {
-			System.out.println("Takogo treugol'nika ne sushchestvuet");
+			System.out.println("This triangle doesn't exist");
 		}
 	}
 
