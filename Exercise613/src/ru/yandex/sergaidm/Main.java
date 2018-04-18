6a177392cc677bf032f96425e51fcaec01aecb04
sergaidm <sergaidm@yandex.ru>
2018-04-18 17:48:36 +0300

    6. Methods. Rectangle drawing method

--- Exercise613/src/ru/yandex/sergaidm/Main.java
+++ Exercise613/src/ru/yandex/sergaidm/Main.java
@@ -7,17 +7,15 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Vvedite dlinu pryamougol'nuka a = ");
+		System.out.println("Input length of rectangle: ");
 		int a = sc.nextInt();
-		System.out.println("Vvedite shirinu pryamougol'nuka b = ");
+		System.out.println("Input width of rectangle: ");
 		int b = sc.nextInt();
-
 		drawRectangle(a, b);
 
 	}
 
 	public static void drawRectangle(int a, int b) {
-
 		for (int i = 1; i <= a; i++) {
 			for (int j = 1; j <= b; j++) {
 				if (i == 1 || i == a || j == 1 || j == b) {
@@ -25,11 +23,9 @@ public class Main {
 				} else {
 					System.out.print(" ");
 				}
-
 			}
 			System.out.println();
 		}
-
 	}
 
 }
