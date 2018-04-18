a7c3f8aff49a99205b68aae1d6f83aac0425541d
sergaidm <sergaidm@yandex.ru>
2018-04-18 16:27:57 +0300

    5. Arrays. Odd digit counting in array

--- Exercise511/src/ru/yandex/sergaidm/Main.java
+++ Exercise511/src/ru/yandex/sergaidm/Main.java
@@ -3,15 +3,15 @@ package ru.yandex.sergaidm;
 public class Main {
 
 	public static void main(String[] args) {
+
 		int[] a = { 0, 5, 2, 4, 7, 1, 3, 19 };
 		int b = 0;
 		for (int i = 0; i < a.length; i++) {
 			if (a[i] % 2 != 0) {
 				b++;
 			}
-
 		}
-		System.out.println("Kolichestvo nechetnyh cyfr v massive = " + b);
+		System.out.println("Number of odd digits in the array = " + b);
 	}
 
 }
