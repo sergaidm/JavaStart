17964cd5097288212edffbef875d92d491015d0e
sergaidm <sergaidm@yandex.ru>
2018-04-18 15:30:48 +0300

    3. Conditional statements. Determination of point belonging to triangle

--- Exercise322/src/ru/yandex/sergaidm/Main.java
+++ Exercise322/src/ru/yandex/sergaidm/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		double x;
 		double y;
 		double ax = 0;
@@ -13,20 +14,17 @@ public class Main {
 		double by = 4;
 		double cx = 6;
 		double cy = 1;
-
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Vvedite koordinaty x i y:");
+		System.out.println("Input coordinates of point x i y: ");
 		x = sc.nextDouble();
 		y = sc.nextDouble();
-
 		double a = (ax - x) * (by - ay) - (bx - ax) * (ay - y);
 		double b = (bx - x) * (cy - by) - (cx - bx) * (by - y);
 		double c = (cx - x) * (ay - cy) - (ax - cx) * (cy - y);
-
 		if (a >= 0 & b >= 0 &  c >= 0 || a <= 0 & b <= 0 & c <= 0) {
-			System.out.println("Vvedennye koordinaty lejat vnutri treugol'nika");
+			System.out.println("This point is located inside the triangle");
 		} else {
-			System.out.println("Vvedennye koordinaty lejat za predelami treugol'nika");
+			System.out.println("This point is located outside the triangle");
 		}
 
 	}
