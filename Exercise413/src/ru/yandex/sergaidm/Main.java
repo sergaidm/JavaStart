083b825d5325939f3e6f53b7920a02bd9a99da36
sergaidm <sergaidm@yandex.ru>
2018-04-18 15:57:56 +0300

    4. Cycles. Multiplication table

--- Exercise413/src/ru/yandex/sergaidm/Main.java
+++ Exercise413/src/ru/yandex/sergaidm/Main.java
@@ -3,6 +3,7 @@ package ru.yandex.sergaidm;
 public class Main {
 
 	public static void main(String[] args) {
+
 		int a = 0;
 		for (int i = 1; i <= 9; i++) {
 			a = i * 5;
@@ -10,5 +11,4 @@ public class Main {
 		}
 
 	}
-
 }
