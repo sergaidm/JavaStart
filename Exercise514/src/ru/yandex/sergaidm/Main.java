4fefb2ced25ed1dd1576a70c3407c842b8fa2ad2
sergaidm <sergaidm@yandex.ru>
2018-04-18 16:49:05 +0300

    5. Arrays. Character counting in the text

--- Exercise514/src/ru/yandex/sergaidm/Main.java
+++ Exercise514/src/ru/yandex/sergaidm/Main.java
@@ -1,6 +1,5 @@
 package ru.yandex.sergaidm;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,21 +7,16 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Napishite predlojenie");
+		System.out.println("Input text: ");
 		String text = sc.nextLine();
-		System.out.println(text);
-
 		char[] array = text.toCharArray();
-		System.out.println(Arrays.toString(array));
-
 		int n = 0;
 		for (int i = 0; i < array.length; i++) {
-				if (array[i] == 'b' || array[i] == 'B') {
-				n += 1;
+			if (array[i] == 'b' || array[i] == 'B') {
+				n++;
 			}
 		}
-
-		System.out.println("Kolichestvo simvolov 'b' v predlojenii = " + n);
+		System.out.println("Number of symbols 'b' in the text: " + n);
 	}
 
 }
