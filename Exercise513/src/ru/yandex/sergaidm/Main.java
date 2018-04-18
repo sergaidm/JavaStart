15b942934d7a68e408bb517a64a0ed957d2248f3
sergaidm <sergaidm@yandex.ru>
2018-04-18 16:39:31 +0300

    5. Arrays. Array of random numbers

--- Exercise513/src/ru/yandex/sergaidm/Main.java
+++ Exercise513/src/ru/yandex/sergaidm/Main.java
@@ -5,18 +5,16 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		int[] a = new int[15];
-		for (int i = 0; i < a.length; i++) {
-			a[i] = (int) (Math.random() * 15);
+		
+		int[] array = new int[15];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) (Math.random() * 15);
 		}
-		System.out.println(Arrays.toString(a));
-
-		int[] b = Arrays.copyOf(a, 30);
-
+		System.out.println(Arrays.toString(array));
+		int[] b = Arrays.copyOf(array, 30);
 		for (int i = 15; i < b.length; i++) {
 			b[i] = 2 * b[i - 15];
 		}
-
 		System.out.println(Arrays.toString(b));
 	}
 
