a84748ee94409e280d18a030f20e1c09f92d65ed
sergaidm <sergaidm@yandex.ru>
2018-04-18 17:58:28 +0300

    6. Methods. Method for counting number of words in the text

--- Exercise615/src/ru/yandex/sergaidm/Main.java
+++ Exercise615/src/ru/yandex/sergaidm/Main.java
@@ -4,18 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Input sentence: ");
+		System.out.println("Input text: ");
 		String text = sc.nextLine();
-
-		System.out.println("Number of words in the sentence = " + countWords(text));
-
+		System.out.println("Number of words in the text: " + countWords(text));
 	}
 
 	public static int countWords(String text) {
-
-		String[] t = text.split("[ ]");
+		String[] t = text.split("[ ,.!?-]");
 		int i = 0;
 		while (i < t.length) {
 			i++;
