0ab81170c6b765794ad08f014afec12c7373c5fe
sergaidm <sergaidm@yandex.ru>
2018-04-18 19:40:50 +0300

    7. Elements of standard library. Own version of method "Arrays.toString"

--- Exercise712/src/ru/yandex/sergaidm/Main.java
+++ Exercise712/src/ru/yandex/sergaidm/Main.java
@@ -14,26 +14,21 @@ public class Main {
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) (Math.random() * 100);
 		}
-
-		System.out.println("Standart version of method Arrays to String: " + Arrays.toString(array));
-		System.out.println("Own version of method Arrays to String:      " + ownVersionOfMethodArraysToString(array));
+		System.out.println("Standart version of method \"Arrays to String\": " + Arrays.toString(array));
+		System.out.println("Own version of method \"Arrays to String\":      " + ArraysToString(array));
 
 	}
 
-	public static String ownVersionOfMethodArraysToString(int[] a) {
-
+	public static String ArraysToString(int[] a) {
 		StringBuilder sb = new StringBuilder("[");
-
 		for (int i = 0; i < a.length; i++) {
 			if (i == a.length - 1) {
 				sb.append(a[i] + "]");
 			} else {
 				sb.append(a[i] + ", ");
 			}
-		}
-
-		String text = sb.toString();
-		return text;
+		}		
+		return sb.toString();
 	}
 
 }
