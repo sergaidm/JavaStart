c6c26ccf56fa02d1c4937df3d9c061cea7a8d2c5
sergaidm <sergaidm@yandex.ru>
2018-04-18 14:32:40 +0300

    2. Variables. Area of triangle

--- Exercise22/src/ru/yandex/sergaidm/Main.java
+++ Exercise22/src/ru/yandex/sergaidm/Main.java
@@ -7,8 +7,8 @@ public class Main {
 		double b = 7;
 		double c = 8;
 		double p = (a + b + c) / 2;
-		double S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-		System.out.println("Ploshchad treugolnika S = " + S);
+		double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		System.out.println("Area of triangle S = " + s);
 
 	}
 
