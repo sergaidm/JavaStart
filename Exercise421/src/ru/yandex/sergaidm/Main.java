d9e24b5bf19d276b69321fea63895c61ee825cf5
sergaidm <sergaidm@yandex.ru>
2018-04-18 16:10:27 +0300

    4. Cycles. Shape with a single cycle

--- Exercise421/src/ru/yandex/sergaidm/Main.java
+++ Exercise421/src/ru/yandex/sergaidm/Main.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		int i = 0;
 		int j = 0;
 		int h;
-		System.out.println("Vvedite vysotu figury");
+		System.out.println("Input the height of shape: ");
 		h = sc.nextInt();
-		
-		while (i<2*h-1) {
-			if (j>=(h-Math.abs(h-i))) {
+		while (i < 2 * h - 1) {
+			if (j >= (h - Math.abs(h - i))) {
 				System.out.println();
 				i++;
-				j=0;
-			} System.out.print("*");
+				j = 0;
+			}
+			System.out.print("*");
 			j++;
 		}
 
 	}
-
 }
