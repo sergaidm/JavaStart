9a535b4b6532347d9241a9ef72c30149d0fcb110
sergaidm <sergaidm@yandex.ru>
2018-04-18 15:25:24 +0300

    3. Conditional statements. Determination of point belonging to circle

--- Exercise321/src/ru/yandex/sergaidm/Main.java
+++ Exercise321/src/ru/yandex/sergaidm/Main.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		int r = 4;
 		double x;
 		double y;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Vvedite koordinaty x i y:");
+		System.out.println("Input coordinates of point x i y: ");
 		x = sc.nextDouble();
 		y = sc.nextDouble();
 		if (Math.pow(x, 2) + Math.pow(y, 2) <= Math.pow(r, 2)) {
-			System.out.println("Vvedennye koordinaty lejat vnutri kruga");
+			System.out.println("This point is located inside the circle");
 		} else {
-			System.out.println("Vvedennye koordinaty lejat za predelami kruga");
+			System.out.println("This point is located outside the circle");
 		}
 
 	}
