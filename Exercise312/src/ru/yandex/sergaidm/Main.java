d5520b494cb436a3b2e41ca62d055107b6e26b7c
sergaidm <sergaidm@yandex.ru>
2018-04-18 15:01:04 +0300

    3. Conditional statements. Determination of floor and  entrance of flat

--- Exercise312/src/ru/yandex/sergaidm/Main.java
+++ Exercise312/src/ru/yandex/sergaidm/Main.java
@@ -5,23 +5,21 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		int flat;
 		int frontDoor;
 		int floor;
-		System.out.println("Vvedite nomer kvartiry flat = ");
+		System.out.println("Input flat number: ");
 		flat = sc.nextInt();
-
 		if (flat >= 1 & flat <= 4 * 4 * 9) {
 			frontDoor = ((flat - 1) / (9 * 4)) + 1;
 			flat = flat - 36 * (frontDoor - 1);
 			floor = (flat + 3) / 4;
-
-			System.out.println("Kvartira raspolojena v pod'ezde " + frontDoor);
-			System.out.println("Kvartira raspolojena na etaje " + floor);
-
+			System.out.println("Flat is located in " + frontDoor + " entrance");
+			System.out.println("Flat is located on " + floor + " floor");
 		} else {
-			System.out.println("V etom dome takoi kvartiry net");
+			System.out.println("There is no such this flat in that house");
 		}
 
 	}
