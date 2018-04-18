fea317cc14f1ad8c7c42a97bed50082787e8e21b
sergaidm <sergaidm@yandex.ru>
2018-04-18 17:11:59 +0300

    5. Arrays. "Mirrored" array

--- Exercise522/src/ru/yandex/sergaidm/Main.java
+++ Exercise522/src/ru/yandex/sergaidm/Main.java
@@ -6,25 +6,23 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Vvedite dlinu massiva n = ");
+		System.out.println("Input size of array n = ");
 		int n = sc.nextInt();
-		int[] a = new int[n];
-		for (int i = 0; i < a.length; i++) {
-			a[i] = (int) (Math.random() * 10);
+		int[] array = new int[n];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) (Math.random() * 10);
 		}
-		System.out.println("Massiv a: " + Arrays.toString(a));
+		System.out.println("Base array: " + Arrays.toString(array));
 
-			for (int i = 0; i < a.length/2; i++) {
-				
-				int p = a[i];
-				a[i] = a[a.length - i - 1];
-				a[a.length - i - 1] = p;
-											
+		for (int i = 0; i < array.length / 2; i++) {
+			int element = array[i];
+			array[i] = array[array.length - i - 1];
+			array[array.length - i - 1] = element;
 		}
-
-		System.out.println("Zerkal'nyi massiv: " + Arrays.toString(a));
-
+		System.out.println("\"Mirrored\" array: " + Arrays.toString(array));
+		
 	}
 
 }
