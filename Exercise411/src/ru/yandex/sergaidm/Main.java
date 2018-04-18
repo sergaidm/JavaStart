503244f7bce5e76f1093eddd225e6d88c9fedd44
sergaidm <sergaidm@yandex.ru>
2018-04-18 15:49:20 +0300

    4. Cycles. Wallpaper

--- Exercise411/src/ru/yandex/sergaidm/Main.java
+++ Exercise411/src/ru/yandex/sergaidm/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		int n;
-		System.out.println("Vvedite kolichestvo polos oboev");
+		System.out.println("Input number of wallpaper stripes: ");
 		n = sc.nextInt();
-
 		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= n; j++) {
 				if (j % 2 == 0) {
@@ -20,5 +20,6 @@ public class Main {
 			}
 			System.out.println();
 		}
+
 	}
 }
