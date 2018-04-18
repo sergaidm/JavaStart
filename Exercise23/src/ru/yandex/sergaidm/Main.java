b605a3cf41ccd88c377ca4b2491c76d66f1fd059
sergaidm <sergaidm@yandex.ru>
2018-04-18 14:38:12 +0300

    2. Variables. Circle length

--- Exercise23/src/ru/yandex/sergaidm/Main.java
+++ Exercise23/src/ru/yandex/sergaidm/Main.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		double r;
-		System.out.println("Vvedite radius okruzhnosti r = ");
+		System.out.println("Input radius of circle r = ");
 		r = sc.nextDouble();
 		System.out.println(r);
 		double c = 2 * Math.PI * r;
-		System.out.println("Dlina okruzhnosti c = " + c);
+		System.out.println("Circle length c = " + c);
 
 	}
 
