d0223683f2b12f51ce6afc389d49a0addc2ab9fe
sergaidm <sergaidm@yandex.ru>
2018-04-19 09:12:48 +0300

    8. Working with files. Method of reading two-dimensional array from file

---
+++ Exercise821/src/ru/yandex/sergaidm/Main.java
@@ -0,0 +1,63 @@
+package ru.yandex.sergaidm;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[][] array = loadArrayFromFile("Matrix.txt");
+		for (int[] rows : array) {
+			for (int element : rows) {
+				System.out.print(element + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+	static int[][] loadArrayFromFile(String fileName) {
+		File fLoad = new File(fileName);
+		if (!fLoad.exists() || fLoad.isDirectory()) {
+			System.out.println("File not found. Returns a zero-size array");
+			return new int[0][0];
+		}
+		int i = 0;
+		int j = 0;
+		int[][] array = new int[calculateArraySize(fLoad)[0]][calculateArraySize(fLoad)[1]];
+		try (BufferedReader br = new BufferedReader(new FileReader(fLoad))) {
+			String tempLine = "";
+			for (; (tempLine = br.readLine()) != null;) {
+				String[] cols = tempLine.split(" ");
+				for (String rows : cols) {
+					array[i][j++] = Integer.valueOf(rows);
+				}
+				i++;
+				j = 0;
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		return array;
+	}
+
+	static int[] calculateArraySize(File file) {
+		int[] array = { 0, 0 };
+		int n = 0;
+		int k = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String tempLine = "";
+			for (; (tempLine = br.readLine()) != null;) {
+				n = tempLine.split(" ").length;
+				k++;
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		array[0] = k;
+		array[1] = n;
+		return array;
+	}
+
+}
