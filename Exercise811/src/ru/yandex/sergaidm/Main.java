1d0511d967e724ee35da3f1a30ec82c26fd7534a
sergaidm <sergaidm@yandex.ru>
2018-04-19 08:20:29 +0300

    8. Working with files. Console text editor

--- Exercise811/src/ru/yandex/sergaidm/Main.java
+++ Exercise811/src/ru/yandex/sergaidm/Main.java
@@ -1,7 +1,5 @@
 package ru.yandex.sergaidm;
 
-//Create a console text editor with the ability to save typed text to a file.
-
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,24 +10,23 @@ public class Main {
 	public static void main(String[] args) {
 
 		File textRedactor = new File("Notepade.txt");
-
 		try {
 			textRedactor.createNewFile();
 		} catch (IOException e) {
 			System.out.println("Error file create");
 		}
-
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Input a text");
-
-		String text = sc.nextLine();
-
+		System.out.println("Input a text. For exit input \"Exit\": ");
 		try (PrintWriter pw = new PrintWriter("Notepade.txt")) {
-			pw.println(text);
+			String text = sc.nextLine();
+			for (; text.compareToIgnoreCase("exit") != 0;) {
+				pw.println(text);
+				text = sc.nextLine();
+			}
 		} catch (IOException e) {
 			System.out.println("Error file write");
 		}
-
 		System.out.println("Your text saved in file Notepade.txt");
+
 	}
 }
