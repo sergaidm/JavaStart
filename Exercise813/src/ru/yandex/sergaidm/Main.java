a63e82c0a1c280881ea86695895b193af5a50592
sergaidm <sergaidm@yandex.ru>
2018-04-19 08:35:52 +0300

    8. Working with files. Method of displaying all catalogs from catalog

--- Exercise813/src/ru/yandex/sergaidm/Main.java
+++ Exercise813/src/ru/yandex/sergaidm/Main.java
@@ -1,41 +1,24 @@
 package ru.yandex.sergaidm;
 
-//Create a method that displays a list of all the directories 
-//contained in the directory that is the parameter of this method.
-
 import java.io.File;
 
 public class Main {
 
 	public static void main(String[] args) {
+		printFolderInfo("/E:\\");
+	}
 
-		File folder = new File(".");
-
-		File folderTwo = new File("Folder");
-		folderTwo.mkdirs();
-
-		int n = 1;
-		while (n <= 10) {
-			createFolders(n);
-			n++;
+	public static void printFolderInfo(String folderName) {
+		File folder = new File(folderName);
+		if (!folder.exists() || !folder.isDirectory()) {
+			System.out.println("Illegal arguments");
+			return;
 		}
-
-		listOfFolders(folderTwo);
-
-	}
-
-	public static void createFolders(int n) {
-		File folder = new File("Folder/Folder" + n);
-		folder.mkdir();
-
-	}
-
-	public static void listOfFolders(File folder) {
-
-		File[] files = folder.listFiles();
-		for (File file : files) {
-			if (file.isDirectory()) {
-				System.out.println(file);
+		String[] fileList = folder.list();
+		for (String fileName : fileList) {
+			File tempfile = new File(folder, fileName);
+			if (tempfile.isDirectory()) {
+				System.out.println(fileName);
 			}
 		}
 	}
